2991cd1036b608f788be9c0dbb153685a495e557
Luciano Seguel <lucianitoseguel@gmail.com>
2025-05-06 21:53:00 -0300

    starting

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,50 +1,57 @@
 using System;
 
 class Program
+
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep5 World!");
-        DisplayWelcomeMessage();
+        DisplayWelcome();
 
-        string userName = PromptUserName();
-        int userNumber = PromptUserNumber();
+        string username = PromptUserName();
+        int age = PromptUserNumber(); 
+        double square = SquareNumber(age);
+        DisplayResult(username,age,square);
 
-        int squaredNumber = SquareNumber(userNumber);
 
-        DisplayResult(userName, squaredNumber);
-    }
-
-    static void DisplayWelcomeMessage()
-    {
-        Console.WriteLine("Welcome to the program!");
-    }
-
-    static string PromptUserName()
-    {
-        Console.Write("Please enter your name: ");
-        string name = Console.ReadLine();
-
-        return name;
-    }
-
-    static int PromptUserNumber()
-    {
-        Console.Write("Please enter your favorite number: ");
-        int number = int.Parse(Console.ReadLine());
-
-        return number;
-    }
-
-    static int SquareNumber(int number)
-    {
-        int square = number * number;
-        return square;
-    }
-
-    static void DisplayResult(string name, int square)
-    {
-        Console.WriteLine($"{name}, the square of your number is {square}");
-    }
     
+    }
+// Display Initial message 
+static void DisplayWelcome (){
+    Console.WriteLine("Welcome to the Program!");
+}
+// Ask for username 
+static string PromptUserName(){
+    Console.Write("Please enter your name: ");
+    string name =  Console.ReadLine();
+
+    return name;
+}
+//Ask to user for a number
+static int PromptUserNumber(){
+    Console.Write("What is your age? ");
+    int age = int.Parse(Console.ReadLine());
+    return age;
+
+}
+
+//Ask for a square number
+static double SquareNumber( int number){
+    Console.Write("Write your square number " );
+    int squareNumber = int.Parse(Console.ReadLine()) ;
+    double square = Math.Pow( number, squareNumber);
+
+    return square;
+
+}
+
+
+
+//display the final result
+static void DisplayResult (string name, int age, double square){
+
+    Console.WriteLine($"Nice to meet you {name} you are {age} and your square number is {square}");
+    
+}
+
 }
